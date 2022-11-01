6c6b48e715218c72e82cde57d9d936bed3b34783
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-01 20:58:54 -0300

    fix: mudança na chave de abertura da classe TrilhasDAO, da linha 5 para a linha 39

--- src/DAO/trilhas-DAO.js
+++ src/DAO/trilhas-DAO.js
@@ -2,40 +2,39 @@ class TrilhasDAO {
   constructor(dbTrilhas) {
     this.dbTrilhas = dbTrilhas;
   }
-}
 
-getAll = () => {
-  return new Promise((resolve, reject) => {
-    this.dbTrilhas.all("SELECT * FROM TRILHAS", (error, rows) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve({
-          trilhas: rows,
-          error: false,
-        });
-      }
-    });
-  });
-};
-
-getById = (id) => {
-  return new Promise((resolve, reject) => {
-    this.dbTrilhas.get(
-      "SELECT * FROM TRILHAS WHERE ID = ?",
-      id,
-      (error, row) => {
+  getAll = () => {
+    return new Promise((resolve, reject) => {
+      this.dbTrilhas.all("SELECT * FROM TRILHAS", (error, rows) => {
         if (error) {
           reject(error);
         } else {
           resolve({
-            trilha: row,
+            trilhas: rows,
             error: false,
           });
         }
-      }
-    );
-  });
-};
+      });
+    });
+  };
 
+  getById = (id) => {
+    return new Promise((resolve, reject) => {
+      this.dbTrilhas.get(
+        "SELECT * FROM TRILHAS WHERE ID = ?",
+        id,
+        (error, row) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve({
+              trilha: row,
+              error: false,
+            });
+          }
+        }
+      );
+    });
+  };
+}
 export default TrilhasDAO;
