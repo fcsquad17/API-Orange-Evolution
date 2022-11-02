dfa3a241b82b2d6a188838623397ead681f35ddd
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-02 01:50:08 -0300

    feat: importação do banco de dados e o controller da entidade users no app.js, chamada da função usersController feita

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import trilhasController from "./controller/trilhas-controller.js";
+import usersController from "./controller/users-controller.js";
 import dbTrilhas from "./database/db-trilhas.js";
+import dbUsers from "./database/db-users.js";
 
 const app = express();
 
 app.use(express.json());
 
 trilhasController(app, dbTrilhas);
+usersController(app, dbUsers);
 
 export default app;
