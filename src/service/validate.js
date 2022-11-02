71c82236abdc574d4281b9981badd3af3d473004
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-02 01:26:54 -0300

    feat: removido parametro id da função validate no arquivo validade.js

--- src/service/validate.js
+++ src/service/validate.js
@@ -1,5 +1,5 @@
-const validate = (nome_completo, email, senha, id_trilhas, admin, id) => {
-  if (nome_completo && email && senha && id_trilhas && admin && id) {
+const validate = (nome_completo, email, senha, id_trilhas, admin) => {
+  if (nome_completo && email && senha && id_trilhas && admin) {
     return true;
   } else {
     throw new Error("Há alguma informação faltando.");
