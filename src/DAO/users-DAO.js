3efbff0cff7e230e94ffc415da45e973651b826b
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-04 16:32:18 -0300

    feat: metodo para pegar as trilhas de determinado usuario apenas pelo id do usuario feito

--- src/DAO/users-DAO.js
+++ src/DAO/users-DAO.js
@@ -132,6 +132,25 @@ class UsersDAO {
     });
   };
 
+  getTrailsByUserId = (idUser) => {
+    return new Promise((resolve, reject) => {
+      this.db.all(
+        "SELECT TRILHAS.TITULO FROM TRILHAS INNER JOIN MODULOS ON MODULOS.TRILHAS_ID = TRILHAS.ID INNER JOIN CONTEUDO ON CONTEUDO.MODULO_ID = MODULOS.ID INNER JOIN USUARIO_CONTEUDO ON CONTEUDO.ID = USUARIO_CONTEUDO.CONTEUDO_ID WHERE USUARIO.ID = ?",
+        idUser,
+        (error, rows) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve({
+              trilhas: rows,
+              error: false,
+            });
+          }
+        }
+      );
+    });
+  };
+
   _verifyId = async (id) => {
     const usuario = await this.getById(id);
     if (usuario.usuario === undefined) {
