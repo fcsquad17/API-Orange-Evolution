f2e1ae520b2d2332f58eb7e3af1e7984cad74553
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-04 15:18:15 -0300

    fix: arquivo de unificação do banco concluidom mudança no nome da variavel db para dbSq

--- src/database/db-sqlite.js
+++ src/database/db-sqlite.js
@@ -3,13 +3,13 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 sqlite3.verbose();
 const filePath = dirname(fileURLToPath(import.meta.url)) + "/database.db";
-const db = new sqlite3.Database(filePath);
+const dbSq = new sqlite3.Database(filePath);
 
 process.on("SIGINT", () =>
-  db.close(() => {
+  dbSq.close(() => {
     console.log("BD encerrado!");
     process.exit(0);
   })
 );
 
-export default db;
+export default dbSq;
