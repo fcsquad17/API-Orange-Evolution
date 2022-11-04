b4c4024f2c1d11e6ecf888a87a903e3e7ee48be1
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-04 19:52:50 -0300

    feat: metodo para busca do primeiro conteudo da trilha pelo id feito

--- src/DAO/content-DAO.js
+++ src/DAO/content-DAO.js
@@ -2,6 +2,33 @@ class ContentDAO {
   constructor(db) {
     this.db = db;
   }
+
+  getFirstContent = (idTrail) => {
+    return new Promise((resolve, reject) => {
+      this.db.get(
+        "SELECT CONTEUDOS.ID FROM CONTEUDOS INNER JOIN MODULOS ON MODULOS_ID = MODULOS.ID INNER JOIN TRILHAS ON TRILHAS.ID = MODULOS.TRILHA_ID WHERE TRILHAS.ID = ? LIMIT 1",
+        idTrail,
+        (error, row) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve({
+              conteudo: row,
+              error: false,
+            });
+          }
+        }
+      );
+    });
+  };
+
+  //   postOneTrailUser = (idTrail, idUser) => {
+  //     return new Promise((resolve, reject) => {
+  //       this.db.run(
+  //         "SELECT * FROM (SELECT ) INSERT INTO CONTEUDO_USUARIO VALUES(?, ?, ?, ?)"
+  //       );
+  //     });
+  //   };
 }
 
 export default ContentDAO;
