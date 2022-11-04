b870ce48fb7f797eb87694ee9f9a295a0a4c0e74
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-04 19:16:38 -0300

    refactor: removido metodo teste de query do app.js

--- src/app.js
+++ src/app.js
@@ -7,26 +7,6 @@ const app = express();
 
 app.use(express.json());
 
-const queryTest = async (db) => {
-  return new Promise((resolve, reject) => {
-    db.all(
-      "SELECT USUARIOS.NOME_COMPLETO, USUARIO_CONTEUDO.DONE, CONTEUDOS.TITULO FROM USUARIOS INNER JOIN USUARIO_CONTEUDO ON USUARIOS.ID = USUARIO_CONTEUDO.USUARIO_ID INNER JOIN CONTEUDOS ON USUARIO_CONTEUDO.CONTEUDO_ID = CONTEUDOS.ID WHERE USUARIO_CONTEUDO.DONE = 0 AND USUARIO_CONTEUDO.USUARIO_ID = 1 ORDER BY USUARIO_CONTEUDO.DONE;",
-      (error, rows) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve({
-            conteudos: rows,
-            error: false,
-          });
-        }
-      }
-    );
-  });
-};
-
-console.log(await queryTest(dbSq));
-
 trilhasController(app, dbSq);
 usersController(app, dbSq);
 
