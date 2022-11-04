4462e5b6c995bb044a24aa0a7eb4791f4548468b
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-04 15:06:56 -0300

    refactor: mudança do filePath para unificação do banco de dados e mudança nas propriedades da entidade usuario_conteudo adicionado chave primaria e atributo done para visualização do progresso do usuario, mais colunas inseridas no banco para testes

--- src/database/create-and-populate-users-content.js
+++ src/database/create-and-populate-users-content.js
@@ -2,24 +2,30 @@ import sqlite3 from "sqlite3";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 sqlite3.verbose();
-const filePath =
-  dirname(fileURLToPath(import.meta.url)) + "/database-users-content.db";
+const filePath = dirname(fileURLToPath(import.meta.url)) + "/database.db";
 const db = new sqlite3.Database(filePath);
 
 const USUARIO_CONTEUDO_SCHEMA = `
 CREATE TABLE IF NOT EXISTS "USUARIO_CONTEUDO" (
+    "ID" INTEGER PRIMARY KEY AUTOINCREMENT,
     "USUARIO_ID" INTEGER NOT NULL,
     "CONTEUDO_ID" INTEGER NOT NULL,
+    "DONE" TINYINT,
     FOREIGN KEY ("USUARIO_ID") REFERENCES "USUARIOS"("ID"),
     FOREIGN KEY ("CONTEUDO_ID") REFERENCES "CONTEUDOS"("ID")
   );`;
 
 const ADD_USUARIO_CONTEUDO_DATA = `
-INSERT INTO "USUARIO_CONTEUDO" (USUARIO_ID, CONTEUDO_ID)
+INSERT INTO "USUARIO_CONTEUDO" (USUARIO_ID, CONTEUDO_ID, DONE)
 VALUES 
-    (1, 1),
-    (2, 2),
-    (3, 3)
+    (1, 1, FALSE),
+    (1, 2, FALSE),
+    (1, 3, FALSE),
+    (2, 1, TRUE),
+    (2, 4, FALSE),
+    (2, 1, TRUE),
+    (2, 2, TRUE),
+    (2, 3, TRUE)
 `;
 
 function criaTabelaUsrCtd() {
