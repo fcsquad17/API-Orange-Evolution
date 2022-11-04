630e9a8db4b7ec6ef7d2daef1ae76dcd7487055a
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-04 20:07:50 -0300

    feat: endpoint para pegar o primeiro conteudo por id da trilha feito no controller da entidade conteudo

--- src/controller/content-controller.js
+++ src/controller/content-controller.js
@@ -2,4 +2,20 @@ import ContentDAO from "../DAO/content-DAO.js";
 
 const contentController = (app, db) => {
   const contentDAO = new ContentDAO(db);
+
+  app.get("/conteudo/idTrilha/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+      await contentDAO._verifyIdTrail(id);
+      const content = await contentDAO.getFirstContent(id);
+      res.status(200).json(content);
+    } catch (e) {
+      res.status(404).json({
+        msg: e.message,
+        error: true,
+      });
+    }
+  });
 };
+
+export default contentController;
