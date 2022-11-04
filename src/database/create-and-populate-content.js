b2b1d4a28a93ebc5ad7d84ed39291fb9bb8fdcca
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-04 15:01:10 -0300

    refactor: mudança do filePath para unificação do banco de dados e mudança nas propriedades da entidade conteudos, adicionado conteudos para população da tabela

--- src/database/create-and-populate-content.js
+++ src/database/create-and-populate-content.js
@@ -2,28 +2,29 @@ import sqlite3 from "sqlite3";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 sqlite3.verbose();
-const filePath =
-  dirname(fileURLToPath(import.meta.url)) + "/database-content.db";
+const filePath = dirname(fileURLToPath(import.meta.url)) + "/database.db";
 const db = new sqlite3.Database(filePath);
 
 const CONTEUDOS_SCHEMA = `
 CREATE TABLE IF NOT EXISTS "CONTEUDOS" (
     "ID" INTEGER PRIMARY KEY AUTOINCREMENT,
-    "TITULO" VARCHAR(100),
+    "TITULO" TEXT,
     "TIPO" VARCHAR(40),
     "DURACAO" INTEGER,
     "FONTE" TEXT,
     "DESCRICAO" TEXT,
     "TAG" VARCHAR(30),
-    "ID_TRILHAS" INTEGER,
-    FOREIGN KEY ("ID_TRILHAS") REFERENCES "TRILHAS"("ID")
+    "MODULO_ID" INTEGER,
+    FOREIGN KEY ("MODULO_ID") REFERENCES "MODULOS"("ID")
   );`;
 
 const ADD_CONTEUDOS_DATA = `
-INSERT INTO "CONTEUDOS" (TITULO, TIPO, DURACAO, FONTE, DESCRICAO, TAG, ID_TRILHAS)
+INSERT INTO "CONTEUDOS" (TITULO, TIPO, DURACAO, FONTE, DESCRICAO, TAG, MODULO_ID)
 VALUES 
   ('Guia definitivo de como migrar para ux design em 5 passos', 'Artigo', '360', 'https://medium.com/orangejuicefc/guia-definitivo-de-como-migrar-para-ux-design-5-passos-para-virar-um-ux-1675f71796b4', 'a', 'migração', 1),
-  ('Design Thinking e carreira: como migrei de Psicologia para UX Design', 'Artigo', '300', 'https://medium.com/orangejuicefc/design-thinking-e-carreira-como-migrei-de-psicologia-para-ux-design-cb79e8b47df5', 'a', 'migração', 1)
+  ('Design Thinking e carreira: como migrei de Psicologia para UX Design', 'Artigo', '300', 'https://medium.com/orangejuicefc/design-thinking-e-carreira-como-migrei-de-psicologia-para-ux-design-cb79e8b47df5', 'a', 'migração', 1),
+  ('De advogada a desenvolvedora: um relato sobre minha migração de carreira e dicas para quem pretende seguir o mesmo caminho', 'Artigo', '360', 'https://medium.com/orangejuicefc/de-advogada-a-desenvolvedora-um-relato-sobre-minha-migração-de-carreira-e-dicas-para-quem-pretende-45ad5df833b5', 'a', 'migração', 2),
+  ('Dev Júnior', 'Artigo', '3.029', 'https://medium.com/orangejuicefc/de-advogada-a-desenvolvedora-um-relato-sobre-minha-migração-de-carreira-e-dicas-para-quem-pretende-45ad5df833b5', 'a', 'migração', 4)
 `;
 
 function criaTabelaCtd() {
