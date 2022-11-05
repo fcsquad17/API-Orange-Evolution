0c946bcc265ec9285bc5d9a0011677af7e6d1177
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-04 22:21:36 -0300

    feat: importação da classe de modelagem do usuario-conteudo para controle do que vai ser mandado para essa tabela, endpoint do post da usuario conteudo feito

--- src/controller/content-controller.js
+++ src/controller/content-controller.js
@@ -1,5 +1,6 @@
 import ContentDAO from "../DAO/content-DAO.js";
-import { validateTrailId } from "../service/validate.js";
+import ContentsUsers from "../model/Contents.js";
+import { validate, validateTrailId } from "../service/validate.js";
 
 const contentController = (app, db) => {
   const contentDAO = new ContentDAO(db);
@@ -12,6 +13,29 @@ const contentController = (app, db) => {
       res.status(200).json(content);
     } catch (e) {
       res.status(404).json({
+        msg: e.message,
+        error: true,
+      });
+    }
+  });
+
+  app.post("/usuario-conteudo/", async (req, res) => {
+    const body = req.body;
+    try {
+      if (validate(...Object.values(body))) {
+        const newContent = new ContentsUsers(...Object.values(body));
+        res
+          .status(201)
+          .json(
+            await contentDAO.postContentUser(
+              newContent.idUser,
+              newContent.idContent,
+              newContent.done
+            )
+          );
+      }
+    } catch (e) {
+      res.status(400).json({
         msg: e.message,
         error: true,
       });
