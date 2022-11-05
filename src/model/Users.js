347a9b768cced701ea751ff0ec5c01cf1989e987
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-04 21:35:30 -0300

    fix: removido atributo id_trilhas da classe de modelagem do usuario

--- src/model/Users.js
+++ src/model/Users.js
@@ -1,9 +1,8 @@
 class Users {
-  constructor(nome_completo, email, senha, id_trilhas, admin) {
+  constructor(nome_completo, email, senha, admin) {
     this.nome_completo = nome_completo;
     this.email = email;
     this.senha = senha;
-    this.id_trilhas = id_trilhas;
     this.admin = admin;
   }
 }
