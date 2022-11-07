770d91e2ea75b8234aaf9581e209b342238b3a94
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-07 19:39:28 -0300

    feat: metodos-queries dos verbos post, put e delete feitos, erro personalizado importado e utilizado na verificação de id

--- src/DAO/trilhas-DAO.js
+++ src/DAO/trilhas-DAO.js
@@ -1,3 +1,5 @@
+import ErrStatus from "../model/Error.js";
+
 class TrilhasDAO {
   constructor(dbTrilhas) {
     this.dbTrilhas = dbTrilhas;
@@ -37,10 +39,69 @@ class TrilhasDAO {
     });
   };
 
+  postTrail = (newTrail) => {
+    return new Promise((resolve, reject) => {
+      this.dbTrilhas.run(
+        "INSERT INTO TRILHAS VALUES(?, ?, ?)",
+        null,
+        newTrail.titulo,
+        newTrail.descricao,
+        (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve({
+              msg: "Trilha criada com sucesso!",
+              trilha: newTrail,
+              error: true,
+            });
+          }
+        }
+      );
+    });
+  };
+
+  putTrail = (id, trail) => {
+    return new Promise((resolve, reject) => {
+      this.dbTrilhas.run(
+        "UPDATE TRILHAS SET TITULO = ?, DESCRICAO = ? WHERE ID = ?",
+        trail.titulo,
+        trail.descricao,
+        id,
+        (error) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve({
+              msg: `Trilha de id ${id} atualizado com sucesso!`,
+              trilha: trail,
+              error: false,
+            });
+          }
+        }
+      );
+    });
+  };
+
+  deleteTrail = (id) => {
+    return new Promise((resolve, reject) => {
+      this.dbTrilhas.run("DELETE FROM TRILHAS WHERE ID = ?", id, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve({
+            msg: `Trilha de id ${id} deletado com sucesso!`,
+            error: false,
+          });
+        }
+      });
+    });
+  };
+
   _verifyId = async (id) => {
     const trilha = await this.getById(id);
     if (trilha.trilha === undefined) {
-      throw new Error(`Trilha de id ${id} não encontrado.`);
+      throw new ErrStatus(`Trilha de id ${id} não encontrado.`, 404);
     }
     return trilha;
   };
