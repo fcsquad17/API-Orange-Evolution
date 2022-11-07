c4182006597143bb8e152d9b8b6df256cea80b90
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-07 16:21:33 -0300

    feat: classe construtora de conteudos feito

--- src/model/Contents.js
+++ src/model/Contents.js
@@ -6,4 +6,16 @@ class ContentsUsers {
   }
 }
 
-export default ContentsUsers;
+class Contents {
+  constructor(titulo, tipo, duracao, fonte, descricao, tag, moduloId) {
+    this.titulo = titulo;
+    this.tipo = tipo;
+    this.duracao = duracao;
+    this.fonte = fonte;
+    this.descricao = descricao;
+    this.tag = tag;
+    this.moduloId = moduloId;
+  }
+}
+
+export { ContentsUsers, Contents };
