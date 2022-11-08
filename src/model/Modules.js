77648320ad7689de89615e3b6f9cab2abeedb4a5
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-08 16:33:03 -0300

    feat: classe construtora do objeto Modules feito

---
+++ src/model/Modules.js
@@ -0,0 +1,9 @@
+class Modules {
+  constructor(titulo, descricao, moduloId) {
+    this.titulo = titulo;
+    this.descricao = descricao;
+    this.moduloId = moduloId;
+  }
+}
+
+export default Modules;
