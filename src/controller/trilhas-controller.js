c774d2ab0c4199395170ed5f7693d790451bdae0
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-07 21:01:10 -0300

    feat: endpoints e validações da entidade trilhas concluidas

--- src/controller/trilhas-controller.js
+++ src/controller/trilhas-controller.js
@@ -1,4 +1,6 @@
 import TrilhasDAO from "../DAO/trilhas-DAO.js";
+import Trails from "../model/Trails.js";
+import { validateBodyTrail } from "../service/validateTrails.js";
 
 const trilhasController = (app, dbTrilhas) => {
   const trilhasDAO = new TrilhasDAO(dbTrilhas);
@@ -20,6 +22,54 @@ const trilhasController = (app, dbTrilhas) => {
       await trilhasDAO._verifyId(id);
       const trilha = await trilhasDAO.getById(id);
       res.status(201).json(trilha);
+    } catch (e) {
+      res.status(404).json({
+        msg: e.message,
+        error: true,
+      });
+    }
+  });
+
+  app.post("/trilhas", async (req, res) => {
+    const body = req.body;
+
+    try {
+      if (validateBodyTrail(...Object.values(body))) {
+        const newTrail = new Trails(...Object.values(body));
+        res.status(201).json(await trilhasDAO.postTrail(newTrail));
+      }
+    } catch (e) {
+      res.status(400).json({
+        msg: e.message,
+        error: true,
+      });
+    }
+  });
+
+  app.put("/trilhas/id/:id", async (req, res) => {
+    const body = req.body;
+    const id = req.params.id;
+
+    try {
+      if (validateBodyTrail(...Object.values(body))) {
+        await trilhasDAO._verifyId(id);
+        const updatedTrail = new Trails(...Object.values(body));
+        res.status(200).json(await trilhasDAO.putTrail(updatedTrail));
+      }
+    } catch (e) {
+      res.status(e.status).json({
+        msg: e.message,
+        error: true,
+      });
+    }
+  });
+
+  app.delete("/trilhas/id/:id", async (req, res) => {
+    const id = req.params.id;
+
+    try {
+      await trilhasDAO._verifyId(id);
+      res.status(200).json(await trilhasDAO.deleteTrail(id));
     } catch (e) {
       res.status(404).json({
         msg: e.message,
