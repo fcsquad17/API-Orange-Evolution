ff9c58488b338d5de0178ea7decdf4249ec1f829
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-08 23:48:10 -0300

    feat: população com todos as trilhas concluido

--- src/database/create-and-populate-trilhas.js
+++ src/database/create-and-populate-trilhas.js
@@ -15,9 +15,21 @@ CREATE TABLE IF NOT EXISTS "TRILHAS" (
 const ADD_TRILHAS_DATA = `
 INSERT INTO "TRILHAS" (TITULO, DESCRICAO)
 VALUES 
-    ('Desenvolvimento Full-Stack', ''),
-    ('UX Design', ''),
-    ('Quality Assurance', '')
+    ('Desenvolvimento Full-Stack', 'Se você chegou até aqui, é porque quer aprender mais sobre tecnologia, especialmente sobre Desenvolvimento Full Stack!
+
+    O Orange Evolution consiste em trilhas totalmente gratuitas para que você possa iniciar a sua carreira na tecnologia. Você terá acesso a vídeos, lives, artigos, apostilas e até cursos gratuitos, além desses conteúdos serem da Orange Juice, de parceiros e empresas que confiamos. 
+    
+    Essa trilha foi montada pensando em quem está começando na área, ou passando por uma migração de carreira e ainda não sabe exatamente o que é esse mundo. Então, aperta o cinto e vem com a gente nessa jornada!'),
+    ('UX Design', 'Se você chegou até aqui, é porque quer aprender mais sobre tecnologia, especialmente sobre UX/UI Design!
+
+    O Orange Evolution consiste em trilhas totalmente gratuitas para que você possa iniciar a sua carreira na tecnologia. Você terá acesso a vídeos, lives, artigos, apostilas e até cursos gratuitos, além desses conteúdos serem da Orange Juice, de parceiros e empresas que confiamos. 
+    
+    Essa trilha foi montada pensando em quem está começando na área, ou passando por uma migração de carreira e ainda não sabe exatamente o que é esse mundo. Então, aperta o cinto e vem com a gente nessa jornada!'),
+    ('Quality Assurance', 'Se você chegou até aqui, é porque quer aprender mais sobre tecnologia, especialmente sobre QA!
+
+    O Orange Evolution consiste em trilhas totalmente gratuitas para que você possa iniciar a sua carreira na tecnologia. Você terá acesso a vídeos, lives, artigos, apostilas e até cursos gratuitos, além desses conteúdos serem da Orange Juice, de parceiros e empresas que confiamos. 
+    
+    Essa trilha foi montada pensando em quem está começando na área, ou passando por uma migração de carreira e ainda não sabe exatamente o que é esse mundo. Então, aperta o cinto e vem com a gente nessa jornada!')
 `;
 
 function criaTabelaTri() {
