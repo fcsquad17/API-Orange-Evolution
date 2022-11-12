3376591b03967a5c95a6e35b2a086b5a07f7541f
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-12 14:22:50 -0300

    refactor: descrição das trilhas padronizada e reduzida para melhorar o card no front

--- src/database/create-and-populate-trilhas.js
+++ src/database/create-and-populate-trilhas.js
@@ -15,21 +15,9 @@ CREATE TABLE IF NOT EXISTS "TRILHAS" (
 const ADD_TRILHAS_DATA = `
 INSERT INTO "TRILHAS" (TITULO, DESCRICAO)
 VALUES 
-    ('Desenvolvimento Full-Stack', 'Se você chegou até aqui, é porque quer aprender mais sobre tecnologia, especialmente sobre Desenvolvimento Full Stack!
-
-    O Orange Evolution consiste em trilhas totalmente gratuitas para que você possa iniciar a sua carreira na tecnologia. Você terá acesso a vídeos, lives, artigos, apostilas e até cursos gratuitos, além desses conteúdos serem da Orange Juice, de parceiros e empresas que confiamos. 
-    
-    Essa trilha foi montada pensando em quem está começando na área, ou passando por uma migração de carreira e ainda não sabe exatamente o que é esse mundo. Então, aperta o cinto e vem com a gente nessa jornada!'),
-    ('UX Design', 'Se você chegou até aqui, é porque quer aprender mais sobre tecnologia, especialmente sobre UX/UI Design!
-
-    O Orange Evolution consiste em trilhas totalmente gratuitas para que você possa iniciar a sua carreira na tecnologia. Você terá acesso a vídeos, lives, artigos, apostilas e até cursos gratuitos, além desses conteúdos serem da Orange Juice, de parceiros e empresas que confiamos. 
-    
-    Essa trilha foi montada pensando em quem está começando na área, ou passando por uma migração de carreira e ainda não sabe exatamente o que é esse mundo. Então, aperta o cinto e vem com a gente nessa jornada!'),
-    ('Quality Assurance', 'Se você chegou até aqui, é porque quer aprender mais sobre tecnologia, especialmente sobre QA!
-
-    O Orange Evolution consiste em trilhas totalmente gratuitas para que você possa iniciar a sua carreira na tecnologia. Você terá acesso a vídeos, lives, artigos, apostilas e até cursos gratuitos, além desses conteúdos serem da Orange Juice, de parceiros e empresas que confiamos. 
-    
-    Essa trilha foi montada pensando em quem está começando na área, ou passando por uma migração de carreira e ainda não sabe exatamente o que é esse mundo. Então, aperta o cinto e vem com a gente nessa jornada!')
+    ('Desenvolvimento Full-Stack', 'Essa trilha foi montada pensando em quem está começando na área, ou passando por uma migração de carreira e ainda não sabe exatamente o que é esse mundo. Então, aperta o cinto e vem com a gente nessa jornada!'),
+    ('UX Design', 'Essa trilha foi montada pensando em quem está começando na área, ou passando por uma migração de carreira e ainda não sabe exatamente o que é esse mundo. Então, aperta o cinto e vem com a gente nessa jornada!'),
+    ('Quality Assurance', 'Essa trilha foi montada pensando em quem está começando na área, ou passando por uma migração de carreira e ainda não sabe exatamente o que é esse mundo. Então, aperta o cinto e vem com a gente nessa jornada!')
 `;
 
 function criaTabelaTri() {
