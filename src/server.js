19b64c70adeb74620ede3bbb66386ace1988b483
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-15 20:56:26 -0300

    refactor: console log notificando a abertura do servidor traduzido para ingles

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ import app from "./app.js";
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Servidor aberto em http://localhost:${port}/`);
+  console.log(`Server is running on port: ${port}`);
 });
