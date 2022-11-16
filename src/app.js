e41ddff21e20a97fcf246cd7bb35a4067b55152e
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-15 21:51:55 -0300

    feat: adicionado configuração do dotenv ao app.js para utilização do process.env

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ import trilhasController from "./controller/trilhas-controller.js";
 import usersController from "./controller/users-controller.js";
 import contentController from "./controller/content-controller.js";
 import modulesController from "./controller/modules-controller.js";
+import * as dotenv from "dotenv";
 import dbSq from "./database/db-sqlite.js";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
